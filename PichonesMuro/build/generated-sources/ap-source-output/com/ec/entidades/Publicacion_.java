c05c54bb7aff608ebde4c30e17392e329b5a2a29
David Tayupanta <davidotb91@hotmail.com>
2016-02-12 21:16:01 -0500

    MuroWebPichones

---
+++ PichonesMuro/build/generated-sources/ap-source-output/com/ec/entidades/Publicacion_.java
@@ -0,0 +1,15 @@
+package com.ec.entidades;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-01-29T09:03:45")
+@StaticMetamodel(Publicacion.class)
+public class Publicacion_ { 
+
+    public static volatile SingularAttribute<Publicacion, Integer> idPublicacion;
+    public static volatile SingularAttribute<Publicacion, String> lugGeoPublicacion;
+    public static volatile SingularAttribute<Publicacion, String> descripconPublicacion;
+
+}
